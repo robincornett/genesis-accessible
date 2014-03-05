a2f3cfefb266472db9b971fab14ac65b2004e858
Rian Rietveld <rian@rrwd.nl>
2014-03-05 09:42:12 +0100

    Bug fix
    headings now visible on alle archives

--- includes/headings.php
+++ includes/headings.php
@@ -8,11 +8,10 @@
 */
 
 
-/** add an H1 on date archives */
+/** add an H1 on archives */
 add_action ('genesis_before_loop', 'wpaccgen_genesis_do_archive_title', 100);
 function wpaccgen_genesis_do_archive_title() {
-	if ( is_category() || is_tag() || is_tax() || is_author() || is_post_type_archive() ) return;
-	if ( is_archive() ) {
+	if ( is_category() || is_tag() || is_tax() || is_author() || is_post_type_archive() || is_archive() ) {
 		remove_filter( 'wp_title', 'genesis_doctitle_wrap', 20 );
   		$title = wp_title( "", false, "" );
 		echo '<div class="archive-description"><h1 class="archive-title">' . $title  . "</h1></div>\n";
@@ -23,7 +22,7 @@ function wpaccgen_genesis_do_archive_title() {
 /** Add an H2 heading to the primary navigation */
 add_filter( 'genesis_do_nav', 'wpaccgen_genesis_add_header_to_primary_nav', 10, 1 );
 function wpaccgen_genesis_add_header_to_primary_nav( $nav_output ) {
-        echo '<h2 class="screen-reader-text">'. __( 'Main navigation', GENWPACC_DOMAIN ) .'</h2>';
+    echo '<h2 class="screen-reader-text">'. __( 'Main navigation', GENWPACC_DOMAIN ) .'</h2>';
     return $nav_output;
 }
 
