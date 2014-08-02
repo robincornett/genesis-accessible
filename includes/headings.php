350144223c182f6632ce44970be7a3a6016ea40f
Rian Rietveld <rian@rrwd.nl>
2014-08-02 11:42:25 +0200

    Moved the H2 Main navigation inside nav
    Moved the H2 Main navigation inside the nav class=nav-primary etc

--- includes/headings.php
+++ includes/headings.php
@@ -8,8 +8,8 @@
 
 
 /** add an H1 on archives */
-add_action ('genesis_before_loop', 'wpaccgen_genesis_do_archive_title', 100);
-function wpaccgen_genesis_do_archive_title() {
+add_action ('genesis_before_loop', 'genwpacc_genesis_do_archive_title', 100);
+function genwpacc_genesis_do_archive_title() {
 	if ( is_category() || is_tag() || is_tax() || is_author() || is_post_type_archive() || is_archive() ) {
 		remove_filter( 'wp_title', 'genesis_doctitle_wrap', 20 );
   		$title = wp_title( "", false, "" );
@@ -19,15 +19,33 @@ function wpaccgen_genesis_do_archive_title() {
 
 
 /** Add an H2 heading to the primary navigation */
-add_filter( 'genesis_do_nav', 'wpaccgen_genesis_add_header_to_primary_nav', 10, 1 );
-function wpaccgen_genesis_add_header_to_primary_nav( $nav_output ) {
-    echo '<h2 class="screen-reader-text">'. __( 'Main navigation', GENWPACC_DOMAIN ) .'</h2>';
-    return $nav_output;
+add_filter( 'genesis_do_nav', 'genwpacc_genesis_add_header_to_primary_nav', 10, 3 );
+function genwpacc_genesis_add_header_to_primary_nav( $nav_output, $nav, $args ) {
+
+    if ( ! genesis_nav_menu_supported( 'primary' ) || ! has_nav_menu( 'primary' ) )
+        return;
+
+    $header =  '<h2 class="screen-reader-text">'. __( 'Main navigation', GENWPACC_DOMAIN ) .'</h2>';
+
+    $nav_markup_open = genesis_markup( array(
+        'html5' => '<nav %s>',
+        'xhtml' => '<div id="nav" class="nav-primary">',
+        'context' => 'nav-primary',
+        'echo' => false,
+    ) );
+
+    $nav_markup_open .= genesis_structural_wrap( 'menu-primary', 'open', 0 );
+    $nav_markup_close = genesis_structural_wrap( 'menu-primary', 'close', 0 );
+    $nav_markup_close .= genesis_html5() ? '</nav>' : '</div>';
+
+    $nav_output = $nav_markup_open . $header . $nav . $nav_markup_close;
+
+    return apply_filters( 'genwpacc_genesis_add_header_to_primary_nav', $nav_output, $nav, $args );
 }
 
 /** Sidebar filter, H4 in Widgets and sidebars modified to an H2 */
-add_filter( 'genesis_register_sidebar_defaults', 'wpaccgen_genesis_register_sidebar_defaults' );
-function wpaccgen_genesis_register_sidebar_defaults( $args ) {
+add_filter( 'genesis_register_sidebar_defaults', 'genwpacc_genesis_register_sidebar_defaults' );
+function genwpacc_genesis_register_sidebar_defaults( $args ) {
 
     $args['before_title'] = '<h2 class="widgettitle widget-title">';
     $args['after_title'] = "</h2>\n";
