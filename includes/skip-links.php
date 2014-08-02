5688cd75924e056d56d9ae6f9e704d50c8899488
Rian Rietveld <rian@rrwd.nl>
2014-08-02 09:41:13 +0200

    Bug fixed, and moved Skip links inside the header
    Removed jump to search from the skip links, there is no way to do this
    properly with the unique id.
    Moved Skip links inside the header. Renamed prefix Renamed classes
    prefix skip-links to genwpacc for consistency.
    Display outline on focus only on links (for IE) and removed the color
    #333,

--- includes/skip-links.php
+++ includes/skip-links.php
@@ -10,8 +10,8 @@
 *
 * @since 1.0.0
 */
-add_action ( 'genesis_before_header', 'wpaccgen_skip_links');
-function wpaccgen_skip_links() {
+add_action ( 'genesis_header', 'genwpacc_skip_links', 5);
+function genwpacc_skip_links() {
 
     $site_layout = genesis_site_layout();
 
@@ -20,13 +20,15 @@ function wpaccgen_skip_links() {
 	$nav2 = false;
     $sidebar = false;
     $sidebar_alt = false;
-	$search = false;
-	$footer_widget = false;
+	$footer = false;
 
 
    	//  navigation?
-   	if ( genesis_get_option( 'menu-primary' ) == '1' ) $nav = true;
-	if ( genesis_get_option( 'menu-secondary' ) == '1' ) $nav2 = true;
+   	echo "menu-primary".has_nav_menu('menu-primary');
+   	if ( genesis_get_option( 'menu-primary' ) == '1' || has_nav_menu( 'primary' ) )
+   		$nav = true;
+	if ( genesis_get_option( 'menu-secondary' ) == '1' || has_nav_menu( 'secondary' ) )
+		$nav2 = true;
 
    	// sidebar?
 	if ( $site_layout == 'sidebar-sidebar-content' || $site_layout == 'content-sidebar-sidebar' || $site_layout == 'sidebar-content-sidebar')  {
@@ -35,11 +37,9 @@ function wpaccgen_skip_links() {
    	}
     if ( $site_layout == 'sidebar-content' || $site_layout == 'content-sidebar' )  $sidebar = true;
 
-	// search widget?
-	if( is_active_widget( '', '', 'search' ) ) $search = true;
 
     // footer widgets?
-    if ( current_theme_supports( 'genesis-footer-widgets' ) == '1' ) {
+    if ( current_theme_supports( 'genesis-footer-widgets' ) == '1' && is_active_sidebar( 'footer-1' ) ) {
     	$footer_widgets = get_theme_support( 'genesis-footer-widgets' );
     	if ( isset( $footer_widgets[0] ) && is_numeric( $footer_widgets[0] ) )
     		$footer = true;
@@ -53,51 +53,58 @@ function wpaccgen_skip_links() {
 
 	if ( function_exists( 'genesis_markup' ) ) {
 
-		add_filter( 'genesis_attr_nav-primary', 'wpacc_genesis_attr_nav_primary' );
-		function wpacc_genesis_attr_nav_primary( $attributes ) {
-    		$attributes['id'] .= 'wpacc-genesis-nav';
+		add_filter( 'genesis_attr_nav-primary', 'genwpacc_genesis_attr_nav_primary' );
+		function genwpacc_genesis_attr_nav_primary( $attributes ) {
+    		$attributes['id'] = 'genwpacc-genesis-nav';
     		return $attributes;
 		}
 
-		add_filter( 'genesis_attr_nav-secondary', 'wpacc_genesis_attr_nav_secondary' );
-		function wpacc_genesis_attr_nav_secondary( $attributes ) {
-    		$attributes['id'] .= 'wpacc-genesis-secondary';
+		add_filter( 'genesis_attr_nav-secondary', 'genwpacc_genesis_attr_nav_secondary' );
+		function genwpacc_genesis_attr_nav_secondary( $attributes ) {
+    		$attributes['id'] = 'genwpacc-genesis-secondary';
     		return $attributes;
 		}
 
-		add_filter( 'genesis_attr_content', 'wpacc_genesis_attr_content' );
-		function wpacc_genesis_attr_content( $attributes ) {
-    		$attributes['id'] .= 'wpacc-genesis-content';
+		add_filter( 'genesis_attr_content', 'genwpacc_genesis_attr_content' );
+		function genwpacc_genesis_attr_content( $attributes ) {
+    		$attributes['id'] = 'genwpacc-genesis-content';
     		return $attributes;
 		}
 
-		add_filter( 'genesis_attr_sidebar-primary', 'wpacc_genesis_attr_sidebar_primary' );
-		function wpacc_genesis_attr_sidebar_primary( $attributes ) {
-    		$attributes['id'] .= 'wpacc-sidebar-primary';
+		add_filter( 'genesis_attr_sidebar-primary', 'genwpacc_genesis_attr_sidebar_primary' );
+		function genwpacc_genesis_attr_sidebar_primary( $attributes ) {
+    		$attributes['id'] = 'genwpacc-sidebar-primary';
     		return $attributes;
 		}
 
-		add_filter( 'genesis_attr_sidebar-secondary', 'wpacc_genesis_attr_sidebar_secondary' );
-		function wpacc_genesis_attr_sidebar_secondary( $attributes ) {
-    		$attributes['id'] .= 'wpacc-sidebar-secondary';
+		add_filter( 'genesis_attr_sidebar-secondary', 'genwpacc_genesis_attr_sidebar_secondary' );
+		function genwpacc_genesis_attr_sidebar_secondary( $attributes ) {
+    		$attributes['id'] = 'genwpacc-sidebar-secondary';
     		return $attributes;
 		}
 
 		if ( !function_exists( 'genesis_attr_footer-widgets' ) ) {
 
-			add_filter( 'genesis_attr_footer-widgets', 'genesis_attr_footer_widgets' );
-			function genesis_attr_footer_widgets( $attributes ) {
+			add_filter( 'genesis_attr_footer-widgets', 'genwpacc_genesis_attr_footer_widgets' );
+			function genwpacc_genesis_attr_footer_widgets( $attributes ) {
 
-					$attributes['id'] = 'wpacc-genesis-footer-widgets';
-     				return $attributes;
+				if ( ! is_active_sidebar( 'footer-1' ) )
+					return;
+
+				$attributes['id'] = 'genwpacc-genesis-footer-widgets';
+     			return $attributes;
 
 			}
 
 		} else {
 
-			add_filter( 'genesis_attr_footer-widgets', 'wpacc_genesis_attr_footer_widgets' );
-			function wpacc_genesis_attr_footer_widgets( $attributes ) {
-    				$attributes['id'] .= 'wpacc-genesis-footer-widgets';
+			add_filter( 'genesis_attr_footer-widgets', 'genwpacc_genesis_attr_footer_widgets' );
+			function genwpacc_genesis_attr_footer_widgets( $attributes ) {
+
+					if ( ! is_active_sidebar( 'footer-1' ) )
+						return;
+
+    				$attributes['id'] .= 'genwpacc-genesis-footer-widgets';
     				return $attributes;
 			}
 		}
@@ -111,21 +118,19 @@ function wpaccgen_skip_links() {
 
     echo '<h2 class="screen-reader-text">'. __( 'Skip links', GENWPACC_DOMAIN ) .'</h2>' . "\n";
 
-	echo '<ul class="wpacc-genesis-skip-link">' . "\n";
+	echo '<ul class="genwpacc_genesis-skip-link" aria-label="'. __( 'Skip links', GENWPACC_DOMAIN ) .'">' . "\n";
 
-    if ($nav) echo '  <li><a href="#wpacc-genesis-nav" class="screen-reader-shortcut">'. __( 'Jump to main navigation', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
+    if ($nav) echo '  <li><a href="#genwpacc-genesis-nav" class="screen-reader-shortcut">'. __( 'Jump to main navigation', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
 
-	if ($search) echo '  <li><a href="#searchform" class="screen-reader-shortcut">'. __( 'Jump to search', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
+	if ($nav2) echo '  <li><a href="#genwpacc-genesis-secondary" class="screen-reader-shortcut">'. __( 'Jump to sub navigation', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
 
-	if ($nav2) echo '  <li><a href="#wpacc-genesis-nav" class="screen-reader-shortcut">'. __( 'Jump to sub navigation', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
+	echo '  <li><a href="#genwpacc-genesis-content" class="screen-reader-shortcut">'. __( 'Jump to content', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
 
-	echo '  <li><a href="#wpacc-genesis-content" class="screen-reader-shortcut">'. __( 'Jump to content', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
+	if ($sidebar) echo '  <li><a href="#genwpacc-sidebar-primary" class="screen-reader-shortcut">'. __( 'Jump to primary sidebar', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
 
-	if ($sidebar) echo '  <li><a href="#wpacc-sidebar-primary" class="screen-reader-shortcut">'. __( 'Jump to primary sidebar', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
+	if ($sidebar_alt) echo '  <li><a href="#genwpacc-sidebar-secondary" class="screen-reader-shortcut">'. __( 'Jump to secondary sidebar', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
 
-	if ($sidebar_alt) echo '  <li><a href="#wwpacc-sidebar-secondary" class="screen-reader-shortcut">'. __( 'Jump to secondary sidebar', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
-
-	if ($footer) echo '  <li><a href="#wpacc-genesis-footer-widgets" class="screen-reader-shortcut">'. __( 'Jump to footer', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
+	if ($footer) echo '  <li><a href="#genwpacc-genesis-footer-widgets" class="screen-reader-shortcut">'. __( 'Jump to footer', GENWPACC_DOMAIN ) .'</a></li>' . "\n";
 
 	echo '</ul>' . "\n";
 
@@ -134,11 +139,11 @@ function wpaccgen_skip_links() {
 add_action( 'wp_enqueue_scripts', 'genwpacc_skiplinks_scripts' );
 function genwpacc_skiplinks_scripts() {
 
-	wp_enqueue_script( 'genwpacc-skiplinks-js',  GENWPACC_PLUGIN_URL . '/js/genwpacc-skiplinks.js' );
+	wp_enqueue_script( 'genwpacc-skiplinks-js',  GENWPACC_PLUGIN_URL . 'js/genwpacc-skiplinks.js' );
 
 	if ( ( 	genesis_get_option( 'genwpacc_skiplinks_css', 'genwpacc-settings' )  == 0 ) ) return;
 
-	wp_register_style( 'genwpacc-skiplinks-css', GENWPACC_PLUGIN_URL. '/css/genwpacc-skiplinks.css' );
+	wp_register_style( 'genwpacc-skiplinks-css', GENWPACC_PLUGIN_URL. 'css/genwpacc-skiplinks.css' );
 	wp_enqueue_style('genwpacc-skiplinks-css');
 }
 
