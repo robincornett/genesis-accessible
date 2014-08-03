a818536f988737c3f04d2b9707562abf55d38849
Rian Rietveld <rian@rrwd.nl>
2014-08-03 13:21:01 +0200

    Compliancy with Genesis 2.1

--- includes/headings.php
+++ includes/headings.php
@@ -6,16 +6,27 @@
 *	License: GPLv2 or later
 */
 
-
 /** add an H1 on archives */
-add_action ('genesis_before_loop', 'genwpacc_genesis_do_archive_title', 100);
-function genwpacc_genesis_do_archive_title() {
-	if ( is_category() || is_tag() || is_tax() || is_author() || is_post_type_archive() || is_archive() ) {
-		remove_filter( 'wp_title', 'genesis_doctitle_wrap', 20 );
-  		$title = wp_title( "", false, "" );
-		echo '<div class="archive-description"><h1 class="archive-title">' . $title  . "</h1></div>\n";
-	}
+add_action ('genesis_before_loop', 'wpaccgen_genesis_do_archive_title', 100);
+function wpaccgen_genesis_do_archive_title() {
+
+    if ( is_page_template( 'page_blog.php' ) || is_author() ) {
+        remove_filter( 'wp_title', 'genesis_doctitle_wrap', 20 );
+        $title = wp_title( "", false, "" );
+        echo '<div class="screen-reader-text"><h1 class="screen-reader-text">' . $title  . "</h1></div>\n";
+    }
+
+    if ( genesis_first_version_compare( '2.0.2', '>' ) )
+        return;
+
+    if ( is_category() || is_tag() || is_tax() || is_post_type_archive() || is_archive() ) {
+        remove_filter( 'wp_title', 'genesis_doctitle_wrap', 20 );
+        $title = wp_title( "", false, "" );
+        echo '<div class="archive-description"><h1 class="archive-title">' . $title  . "</h1></div>\n";
+    }
+
 }
+
 
 
 /** Add an H2 heading to the primary navigation */
@@ -25,7 +36,7 @@ function genwpacc_genesis_add_header_to_primary_nav( $nav_output, $nav, $args ) 
     if ( ! genesis_nav_menu_supported( 'primary' ) || ! has_nav_menu( 'primary' ) )
         return;
 
-    $header =  '<h2 class="screen-reader-text">'. __( 'Main navigation', GENWPACC_DOMAIN ) .'</h2>';
+    $heading =  '<h2 class="screen-reader-text">'. __( 'Main navigation', GENWPACC_DOMAIN ) .'</h2>';
 
     $nav_markup_open = genesis_markup( array(
         'html5' => '<nav %s>',
@@ -38,7 +49,7 @@ function genwpacc_genesis_add_header_to_primary_nav( $nav_output, $nav, $args ) 
     $nav_markup_close = genesis_structural_wrap( 'menu-primary', 'close', 0 );
     $nav_markup_close .= genesis_html5() ? '</nav>' : '</div>';
 
-    $nav_output = $nav_markup_open . $header . $nav . $nav_markup_close;
+    $nav_output = $nav_markup_open . $heading . $nav . $nav_markup_close;
 
     return apply_filters( 'genwpacc_genesis_add_header_to_primary_nav', $nav_output, $nav, $args );
 }
@@ -52,7 +63,5 @@ function genwpacc_genesis_register_sidebar_defaults( $args ) {
 
     return $args;
 }
-
-// disable HTML5 headings strcuture
 
 ?>
