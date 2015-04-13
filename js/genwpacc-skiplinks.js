9f50d5c1a85d5ab0fa0db7efc8596de4b5e2b106
Gary Jones <GaryJones@users.noreply.github.com>
2015-04-13 15:39:25 +0100

    Fix skip links script code standards
    Fix whitespace, use single quotes, add `use strict`.

--- js/genwpacc-skiplinks.js
+++ js/genwpacc-skiplinks.js
@@ -1,23 +1,18 @@
-/*  genesis-accessible skiplinks
-
-	Version: 1.0
-
-	License: GPL-2.0+
-	License URI: http://www.opensource.org/licenses/gpl-license.php
-
+/**
+ * genesis-accessible skiplinks
+ * Version: 1.0
+ * License: GPL-2.0+
+ * License URI: http://www.opensource.org/licenses/gpl-license.php
  */
 
-window.addEventListener("hashchange", function(event) {
+window.addEventListener( 'hashchange', function() {
+	'use strict';
+	var element = document.getElementById( location.hash.substring( 1 ) );
 
-    var element = document.getElementById(location.hash.substring(1));
-
-    if (element) {
-
-        if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
-            element.tabIndex = -1;
-        }
-
-        element.focus();
-    }
-
+	if ( element ) {
+		if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
+			element.tabIndex = -1;
+		}
+		element.focus();
+	}
 }, false);
