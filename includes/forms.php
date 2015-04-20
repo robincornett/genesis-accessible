267cff0891abfef3c875d6e2ff96654eeba5542d
Rian Rietveld <rian@rrwd.nl>
2015-04-20 15:21:08 +0200

    Label search form now links to genesis_search_text
    Fixes #6
    Also made uniform to genesis 2.2

--- includes/forms.php
+++ includes/forms.php
@@ -26,6 +26,7 @@ add_filter( 'get_search_form', 'wpaccgen_get_search_form_uniqueid', 20 );
  * @return string HTML markup.
  */
 function wpaccgen_get_search_form_uniqueid() {
+
 	$search_text = get_search_query() ? apply_filters( 'the_search_query', get_search_query() ) : apply_filters( 'genesis_search_text', __( 'Search this website', 'genesis' ) . '&#x02026;' );
 
 	$button_text = apply_filters( 'genesis_search_button_text', esc_attr__( 'Search', 'genesis' ) );
@@ -34,18 +35,32 @@ function wpaccgen_get_search_form_uniqueid() {
 	$onblur  = "if ('' === this.value) {this.value = '" . esc_js( $search_text ) . "';}";
 
 	//* Generate ramdom id for the search field (n case there are more than one search form on the page)
-	$id = uniqid( 'searchform-' );
+	$form_id = uniqid( 'searchform-' );
 
 	//* Empty label, by default. Filterable.
-	$label = apply_filters( 'genesis_search_form_label', $search_text );
-	$label = '<label for="' . $id . '" class="screen-reader-text">' . $label . "</label>";
+	$label = apply_filters( 'genesis_search_form_label', '' );
+	if ( '' == $label )  {
+		$label = apply_filters( 'genesis_search_text', __( 'Search this website', 'genesis' ) );
+	}
 
 	$value_or_placeholder = ( get_search_query() == '' ) ? 'placeholder' : 'value';
 
-	if ( genesis_html5() )
-		$form = sprintf( '<form method="get" class="search-form" action="%s" role="search">%s<input type="search" name="s" id="%s" %s="%s" /><input type="submit" value="%s" /></form>', home_url( '/' ), $label, $id, $value_or_placeholder, esc_attr( $search_text ), esc_attr( $button_text ) );
-	else
+	if ( genesis_html5() ) {
+
+		$form .= sprintf(
+				'<meta itemprop="target" content="%s"/><label for="%s">%s</label><input itemprop="query-input" type="search" name="s" id="%s" %s="%s" /><input type="submit" value="%s" /></form>',
+				home_url( '/?s={s}' ),
+				esc_attr( $form_id ),
+				esc_html( $label ),
+				esc_attr( $form_id ),
+				$value_or_placeholder,
+				esc_attr( $search_text ),
+				esc_attr( $button_text )
+		);
+
+	} else {
 		$form = sprintf( '<form method="get" class="searchform search-form" action="%s" role="search" >%s<input type="text" value="%s" name="s" id="%s" class="s search-input" onfocus="%s" onblur="%s" /><input type="submit" class="searchsubmit search-submit" value="%s" /></form>', home_url( '/' ), $label, $id, esc_attr( $search_text ), esc_attr( $onfocus ), esc_attr( $onblur ), esc_attr( $button_text ) );
+	}
 
 	return apply_filters( 'genesis_search_form', $form, $search_text, $button_text, $label );
 
