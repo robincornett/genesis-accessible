343455d128dd6d56f1ba420a1b11c21cc41543de
Robin Cornett <hello@robincornett.com>
2019-06-21 08:10:51 -0400

    Escape titles on 404 page

--- templates/404.php
+++ templates/404.php
@@ -19,39 +19,39 @@ function genwpacc_404() {
 
 	echo genesis_html5() ? '<article class="entry">' : '<div class="post hentry">';
 
-	printf( '<h1 class="entry-title">%s</h1>', __( 'Page not found', 'genesis-accessible' ) );
+	printf( '<h1 class="entry-title">%s</h1>', esc_html__( 'Page not found', 'genesis-accessible' ) );
 	echo '<div class="entry-content">';
 
 	?>
-	<p><?php printf( __( 'The page you are looking for no longer exists. Perhaps you can return back to the site\'s <a href="%s">homepage</a> and see if you can find what you are looking for. Or, you can try finding it with the information below.', 'genesis' ), home_url() ); ?></p><?php
+	<p><?php printf( wp_kses_post( __( 'The page you are looking for no longer exists. Perhaps you can return back to the site\'s <a href="%s">homepage</a> and see if you can find what you are looking for. Or, you can try finding it with the information below.', 'genesis' ) ), esc_url( home_url() ) ); ?></p><?php
 
 	echo '<p>' . wpaccgen_get_search_form_uniqueid() . '</p>';
 
 	?>
 
-	<h2><?php _e( 'Sitemap', 'genesis' ); ?></h2>
+	<h2><?php esc_html_e( 'Sitemap', 'genesis' ); ?></h2>
 
-	<h3><?php _e( 'Pages:', 'genesis' ); ?></h3>
+	<h3><?php esc_html_e( 'Pages:', 'genesis' ); ?></h3>
 	<ul>
 		<?php wp_list_pages( 'title_li=' ); ?>
 	</ul>
 
-	<h3><?php _e( 'Categories:', 'genesis' ); ?></h3>
+	<h3><?php esc_html_e( 'Categories:', 'genesis' ); ?></h3>
 	<ul>
 		<?php wp_list_categories( 'sort_column=name&title_li=' ); ?>
 	</ul>
 
-	<h3><?php _e( 'Authors:', 'genesis' ); ?></h3>
+	<h3><?php esc_html_e( 'Authors:', 'genesis' ); ?></h3>
 	<ul>
 		<?php wp_list_authors( 'exclude_admin=0&optioncount=1' ); ?>
 	</ul>
 
-	<h3><?php _e( 'Monthly:', 'genesis' ); ?></h3>
+	<h3><?php esc_html_e( 'Monthly:', 'genesis' ); ?></h3>
 	<ul>
 		<?php wp_get_archives( 'type=monthly' ); ?>
 	</ul>
 
-	<h3><?php _e( 'Recent Posts:', 'genesis' ); ?></h3>
+	<h3><?php esc_html_e( 'Recent Posts:', 'genesis' ); ?></h3>
 	<ul>
 		<?php wp_get_archives( 'type=postbypost&limit=100' ); ?>
 	</ul>
